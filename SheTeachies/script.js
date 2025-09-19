711ed495c097572600080c879b7cd1b6869f745e
rinnbz <nuynnbz@gmail.com>
2025-09-20 02:22:46 +0800

    Add files via upload

---
+++ SheTeachies/script.js
@@ -0,0 +1,6 @@
+indexlet numButtonClicks = 0;
+function buttonClicked() {
+    numButtonClicks = numButtonClicks + 1;
+    document.getElementById("mainDiv").textContent =
+        "Button Clicked times: " + numButtonClicks;
+}
